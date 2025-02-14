de0c1a894d2d917b2c9ccca26f4f83e9bb5af988
rudransmathur <mathur.rudansh@gmail.com>
2025-02-14 08:40:33 +0530

    added files

---
+++ my_app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Path
+import requests
+import json
+from dotenv import load_dotenv
+import os
+app = FastAPI()
+
+load_dotenv()
+API_PROXY_TOKEN = os.getenv("AIPROXY_TOKEN")
+
+@app.post("/run")
+async def post_run(task : str):
+    url = "https://aiproxy.sanand.workers.dev/openai/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {API_PROXY_TOKEN}",
+    }
+    payload = {
+        "model": "gpt-4o-mini",
+        "messages": [
+            { "role": "system", "content": "You are a helpful math tutor. Guide the user through the solution step by step." },
+            { "role": "user", "content": "how can I solve 8x + 7 = -23" }
+        ]
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    print(response.json())
+    return response.json()
