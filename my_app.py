5062bfcbf88ee13620ca750b0426f0fb04490a0f
rudransmathur <mathur.rudansh@gmail.com>
2025-02-14 09:13:23 +0530

    edited task

--- my_app.py
+++ my_app.py
@@ -8,6 +8,7 @@ app = FastAPI()
 load_dotenv()
 API_PROXY_TOKEN = os.getenv("AIPROXY_TOKEN")
 
+
 @app.post("/run")
 async def post_run(task : str):
     url = "https://aiproxy.sanand.workers.dev/openai/v1/chat/completions"
@@ -17,9 +18,39 @@ async def post_run(task : str):
     payload = {
         "model": "gpt-4o-mini",
         "messages": [
-            { "role": "system", "content": "You are a helpful math tutor. Guide the user through the solution step by step." },
-            { "role": "user", "content": "how can I solve 8x + 7 = -23" }
-        ]
+            {
+                "role": "system",
+                "content": "A task would be given to you. Identify if the task is asking to: install uv and run a link. Reply with 'yes' or 'no'."
+            },
+            {
+                "role": "user",
+                "content": task
+            }
+        ],
+        "response_format": {
+            "type": "json_schema",
+            "json_schema": {
+                "name": "interpret_task",
+                "strict":True,
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "steps": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {"explanation": {"type": "string"}, "output": {"type": "string"}},
+                                "required": ["explanation", "output"],
+                                "additionalProperties": False
+                            }
+                        },
+                        "final_answer": {"type": "string"}
+                    },
+                    "required": ["steps", "final_answer"],
+                    "additionalProperties": False
+                }
+            }
+        }
     }
     response = requests.post(url, headers=headers, json=payload)
     print(response.json())
